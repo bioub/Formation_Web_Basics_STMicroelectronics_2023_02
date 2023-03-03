90e9841b1b349c767fe040db0c06eb77619e6be5
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-03-03 16:40:12 +0100

    Correction Ex-Jeu

--- JavaScript/ex-jeu.js
+++ JavaScript/ex-jeu.js
@@ -1,3 +1,24 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+
 // importe l'API readline de Node.js
 const readline = require('readline');
 
@@ -8,16 +29,46 @@ const rl = readline.createInterface({
 });
 
 
-function jouer() {
+function jouer() {
+  if (essais.length) {
+    console.log('Vous avez saisi : ' + essais.join(' - '));
+  }
+
   rl.question('Quel est le nombre ? ', (answer) => {
     console.log('Vous avez saisi', answer);
 
-    // pour rejouer
-    jouer();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // rl.close(); // désenregistre les callback async
-    // le programme va s'arrêter (sinon CTRL-C pour killer)
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Error : il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      jouer();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      jouer();
+    } else {
+      // désenregistre les callback async
+      // le programme va s'arrêter (sinon CTRL-C pour killer)
+      console.log('Gagné');
+      rl.close();
+    }
   });
 }
 
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 jouer();
+
+
+// pile d'appel
+// ^
+// |                                    [question]                  [question]
+// |[question]                          [jouer   ]                  [jouer   ]
+// |[jouer   ]  ..⟳..                   [=>      ]  ..⟳..           [=>      ]
+// +------------------------------------ENTREE-----------------------ENTREE----------------------------------------------------------------> temps
