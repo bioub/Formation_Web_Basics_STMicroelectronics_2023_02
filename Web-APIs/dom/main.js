5a28228d92e4669f3c1b79f12c032afb94072318
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-03-14 13:56:37 +0100

    Exercice DOM

--- Web-APIs/dom/main.js
+++ Web-APIs/dom/main.js
@@ -7,6 +7,14 @@ const inputEl = document.querySelector('.todos-new-input');
 /** @type {HTMLDivElement} */
 const divEl = document.querySelector('.todos-container');
 
+/*
+Exercice 3
+Sélectionner et ecouter le clic de la checkbox du formulaire
+<input type="checkbox" class="todos-toggle-checked">
+Au clic cocher ou décocher toutes les checkbox
+créées dans l'exercice 1 (celles dans divEl)
+*/
+
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
   const itemEl = createTodo({
--- Web-APIs/dom/todos.js
+++ Web-APIs/dom/todos.js
@@ -6,12 +6,40 @@
  * @returns {HTMLDivElement}
  */
 function createTodo(todo) {
+  /*
+  Exercice 1 :
+  Compléter cette fonction de sorte à créer également
+  la checkbox et le bouton comme dans le code HTML suivant :
+  <div data-todo-id="0.32452543">
+    <input type="checkbox">
+    <span>Item 1</span>
+    <button>-</button>
+  </div>
+  Il faut que la checkbox soit cochée si todo.completed === true
+  */
+
   const rowEl = document.createElement('div');
   rowEl.dataset.todoId = todo.id;
 
   const spanEl = document.createElement('span');
   spanEl.innerText = todo.title;
   rowEl.append(spanEl);
+
+  /*
+  Exercice 4 :
+  Ecouter le double-clic de la balise span
+  et transformer la balise span en input (de sorte
+  à pouvoir modifier le contenu)
+  Ecouter l'événement keydown de l'input
+  si l'utilisateur tape sur ENTREE revenir à la balise span
+  */
+
+  /*
+  Exercice 2 :
+  Dans l'exercice vous avez créé un bouton
+  Ecouter le clic de ce bouton et supprimer
+  la ligne parent du bouton au clic
+  */
 
   return rowEl;
 }
